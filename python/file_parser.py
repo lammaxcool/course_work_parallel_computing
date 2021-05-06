d800d38157e33d1c66a6fd6b602c8d59765e48c5
Maxym <66222425+lammaxcool@users.noreply.github.com>
2021-05-06 20:56:48 +0300

    Fix bug with 'ZipFile: ValueError: seek of closed file'

--- python/file_parser.py
+++ python/file_parser.py
@@ -11,22 +11,23 @@ path_to_execute = ['datasets/aclImdb/test/neg/',
                     'datasets/aclImdb/train/neg/', 
                     'datasets/aclImdb/train/pos/', 
                     'datasets/aclImdb/train/unsup/']
-zip_name = '../datasets.zip'
+zip_path = '../datasets.zip'
 dest_path = '../data/'
 
 print(N_list)
 
 if __name__ == '__main__':
 
-    with ZipFile(zip_name, 'r') as zip_obj:
-        for path, indices in zip(path_to_execute, N_list):
-            root = Path(zip_obj, at=path)
-            files_to_cp = list(root.iterdir())[0 : 10]
+    for path, indices in zip(path_to_execute, N_list):
+        zip_obj = ZipFile(zip_path, 'r')
+        root = Path(zip_obj, at=path)
+        files_to_cp = list(root.iterdir())[0 : 10]
 
-            for file in files_to_cp:
-                curr_path = path + file.name
-                with open(dest_path + file.name, 'wb') as dest_file:
-                    with zip_obj.open(curr_path, 'r') as origin_file:
-                        dest_file.write(origin_file.read())
-        
+        for file in files_to_cp:
+            curr_path = path + file.name
+            with open(dest_path + file.name, 'wb') as dest_file:
+                with zip_obj.open(curr_path, 'r') as origin_file:
+                    dest_file.write(origin_file.read())
+    
         print(path + ' ' + 'Done')
+        zip_obj.close()

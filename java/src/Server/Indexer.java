3ff1e07e4248035efd21a3051cf4bdd390783717
Maxym <66222425+lammaxcool@users.noreply.github.com>
2021-05-08 01:50:24 +0300

    Add stop words to single threaded index service

--- java/src/Server/Indexer.java
+++ java/src/Server/Indexer.java
@@ -25,6 +25,22 @@ class IndexService {
 
     private final String filesPath;
     private Path[] files;
+    private static final  String[] stopWords = {"a", "able", "about",
+            "across", "after", "all", "almost", "also", "am", "among", "an",
+            "and", "any", "are", "aren't", "as", "at", "be", "because", "been",
+            "but", "by", "can", "cannot", "could", "couldn't", "dear", "did",
+            "didn't", "do", "don't", "does", "doesn't", "were", "weren't",
+            "either", "else", "ever", "every", "for", "from", "get", "got",
+            "had", "has", "have", "he", "her", "hers", "him", "his", "how",
+            "however", "i", "if", "in", "into", "is", "it", "its", "just",
+            "least", "let", "like", "likely", "may", "me", "might", "most",
+            "must", "my", "neither", "no", "nor", "not", "of", "off", "often",
+            "on", "only", "or", "other", "our", "own", "rather", "said", "say",
+            "says", "she", "should", "since", "so", "some", "than", "that",
+            "the", "their", "them", "then", "there", "these", "they", "this",
+            "tis", "to", "too", "twas", "us", "wants", "was", "wasn't", "we",
+            "what", "when", "where", "which", "while", "who", "whom", "why",
+            "will", "with", "would", "yet", "you", "your"};
     private String regex = "([^a-zA-Z`']+)'*\\1*";
     private final Map<String, Set<String>> index = new HashMap<>();
 
@@ -63,6 +79,7 @@ class IndexService {
                     String text = Files.readString(file);
                     text = text.toLowerCase();
                     Set<String> words = new HashSet<>(Arrays.asList(text.split(regex)));
+                    words.removeAll(Arrays.asList(stopWords));
                     for (String word : words) {
                         String fileName = file.getFileName().toString();
                         if (!index.containsKey(word)) {

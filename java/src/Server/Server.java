eac6246a90467e61c5a2c9c94cf25308f03a6009
Maxym <66222425+lammaxcool@users.noreply.github.com>
2021-05-18 02:49:04 +0300

    Add thread safe singleton

--- java/src/Server/Server.java
+++ java/src/Server/Server.java
@@ -3,6 +3,7 @@ package Server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Collection;
 
 public class Server {
 
@@ -50,4 +51,40 @@ class ClientHandler extends Thread {
         System.out.println("hello");
     }
 
+}
+
+class Index {
+    private static Index instance;
+    private final Indexer indexer;
+
+    private Index(int threadAmount) {
+        indexer = new Indexer("/data");
+        indexer.initIndex(threadAmount);
+    }
+
+    // use to create index and recreate
+    // or get instance
+    public static Index getInstance(int threadAmount) {
+        if (instance == null) {
+            synchronized (Index.class) {
+                instance = new Index(threadAmount);
+            }
+        }
+        return instance;
+    }
+
+    // 4 threads by default
+    // or get instance
+    public static Index getInstance() {
+        if (instance == null) {
+            synchronized (Index.class) {
+                instance = new Index(4);
+            }
+        }
+        return instance;
+    }
+
+    public Collection<String> getFilesByWords(String... words) {
+        return indexer.getFilesByWords(words);
+    }
 }

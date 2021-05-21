4de1cc868ad60aa53616386e863e8422a0aa9738
Maxym <66222425+lammaxcool@users.noreply.github.com>
2021-05-22 02:28:26 +0300

    Implement client loop to work with server

--- java/src/Client/Client.java
+++ java/src/Client/Client.java
@@ -1,17 +1,39 @@
 package Client;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.Collection;
 
 public class Client {
-    public static void main(String[] args) {
 
+    static String ip = "localhost";
+    static int port = 2021;
+
+    public static void main(String[] args) {
+        // choose port and ip or default 2021 localhost
+        if (args.length == 2) {
+            ip = args[0];
+            port = Integer.parseInt(args[1]);
+        }
+
+        ClientService client = new ClientService(ip, port);
+
+        try {
+            client.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
 class ClientService {
+
+    private final String instruction = """
+            Usage:
+            '/find word1 word2 ...' to find files contains this words
+            '/exit' to exit  
+            """;
+    private final String intro = "index: ";
     private Socket clientSocket;
     private final String ip;
     private final int port;
@@ -23,6 +45,44 @@ class ClientService {
     public ClientService(String ip, int port) {
         this.ip = ip;
         this.port = port;
+    }
+
+    public void start() throws IOException {
+        startConnection(ip, port);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.print(intro);
+            String userLine = reader.readLine();
+            String[] dividedUserLine = userLine.split(" ", 2);
+            String command = dividedUserLine[0];
+            if (command.equals("")) {
+                continue;
+            }
+            if (ping()) {
+                if  (command.equals("/find")) {
+                    send("/find");
+                    String[] commandArgs = {""};
+                    // if user doesn't provide words to find
+                    // send empty string
+                    if (dividedUserLine.length > 1) {
+                        commandArgs = dividedUserLine[1].split(" ");
+                    }
+                    send(commandArgs);
+                    Collection<String> listToReceive = receive();
+                    System.out.println(listToReceive);
+                } else if (command.equals("/exit")) {
+                    send("/exit");
+                    stopConnection();
+                    break;
+                } else {
+                    System.out.println("Unknown command");
+                }
+            } else {
+                stopConnection();
+                break;
+            }
+        }
     }
 
     private void startConnection(String ip, int port) {
